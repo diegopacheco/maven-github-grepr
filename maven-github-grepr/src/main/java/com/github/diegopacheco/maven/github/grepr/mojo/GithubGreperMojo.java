bd65ef7e942eb62431ca07101ec142a3bf23b0cc
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-04-12 07:55:04 -0300

    added project for plugin functional black box test

---
+++ maven-github-grepr/src/main/java/com/github/diegopacheco/maven/github/grepr/mojo/GithubGreperMojo.java
@@ -0,0 +1,14 @@
+package com.github.diegopacheco.maven.github.grepr.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Says "Hi" to the user.
+ * @goal sayhi
+ */
+public class GithubGreperMojo extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+		getLog().info("Hello, world.");
+	}
+}
